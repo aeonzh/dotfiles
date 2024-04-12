d01e494158ae3be72698109e7d5104277b9f7958
Zheng He Hu <aeonzh@hotmail.com>
2024-04-12 21:26:24 +0200

    Move to Lazy.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,155 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+    { "romainl/vim-cool" },
+    { "catppuccin/nvim",                        name = "catppuccin", priority = 1000 },
+
+    { "nvim-treesitter/nvim-treesitter",        build = ":TSUpdate" },
+    { "nvim-treesitter/nvim-treesitter-context" },
+
+    {
+        "neovim/nvim-lspconfig",
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'williamboman/mason-lspconfig.nvim' },
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            local lspconfig = require('lspconfig')
+
+            lsp_zero.extend_lspconfig()
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
+
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = { 'lua_ls', 'gopls', 'terraformls', 'yamlls', 'helm_ls', 'ruby_ls' },
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                },
+            })
+
+            lsp_zero.format_on_save({
+                format_opts = {
+                    async = false,
+                    timeout_ms = 10000,
+                },
+                servers = {
+                    ['terraformls'] = { 'terraform', 'terraform-vars' },
+                    ['lua_ls'] = { 'lua' },
+                    ['gopls'] = { 'go' },
+                }
+            })
+
+            lspconfig.lua_ls.setup {}
+            lspconfig.terraformls.setup {}
+            lspconfig.yamlls.setup {
+                settings = {
+                    yaml = {
+                        schemas = {
+                            ["https://raw.githubusercontent.com/CircleCI-Public/circleci-yaml-language-server/main/schema.json"] = "/.circleci/config.yml"
+                        },
+                    },
+                }
+            }
+            lspconfig.helm_ls.setup {
+                settings = {
+                    ['helm-ls'] = {
+                        yamlls = {
+                            path = "yaml-language-server",
+                            schemas = {
+                                ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.28.8-standalone-strict/all.json"] = "/templates/*.yaml"
+                            }
+                        }
+                    }
+                }
+            }
+        end
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        lazy = true,
+        config = false,
+        init = function()
+            vim.g.lsp_zero_extend_cmp = 0
+            vim.g.lsp_zero_extend_lspconfig = 0
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = true
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" },
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
+
+            local cmp = require('cmp')
+            local cmp_action = lsp_zero.cmp_action()
+
+            cmp.setup({
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert'
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+                formatting = lsp_zero.cmp_format({ details = true }),
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<Tab>'] = cmp_action.luasnip_supertab(),
+                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+            })
+        end
+    },
+
+    { "saadparwaiz1/cmp_luasnip" },
+
+    { dir = "/opt/homebrew/opt/fzf" },
+    { "junegunn/fzf.vim" },
+    { "junegunn/vim-peekaboo" },
+    { "junegunn/goyo.vim",          ft = "markdown" },
+
+    { "plasticboy/vim-markdown",    ft = "markdown" },
+    { "towolf/vim-helm",            ft = "helm" },
+
+    { "lilydjwg/colorizer",         cmd = "ColorHighlight" }
+})
+
 vim.cmd([[
-    set runtimepath^=$HOME/.vim runtimepath+=$HOME/.vim/after
+    set runtimepath^=$HOME/.vim
     let &packpath = &runtimepath
     source $HOME/.vim/vimrc
 ]])
--- .zshrc
+++ .zshrc
@@ -30,8 +30,8 @@ setopt HIST_VERIFY
 setopt SHARE_HISTORY
 
 # FZF
-[ -f "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh ] && source "${XDG_CONFIG_HOME:-$HOME/.config}"/fzf/fzf.zsh
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
+eval "$(fzf --zsh)"
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow"
 export FZF_DEFAULT_OPTS='--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'
 
 # NNN
